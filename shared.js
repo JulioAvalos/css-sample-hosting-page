240d9a2a067cf7ec01128ef7dd626f30c0ee8218
JulioAvalos <julioavalos2@msn.com>
2021-12-22 11:45:57 -0600

    transitions and display

--- shared.js
+++ shared.js
@@ -12,6 +12,9 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // modal.className = 'open'; // this will actually overwrite the complete class list
     modal.classList.add('open');
     backdrop.classList.add('open');
+    setTimeout(function () {
+      backdrop.style.display = 'block';
+    }, 200);
   });
 }
 
@@ -21,23 +24,28 @@ backdrop.addEventListener('click', function () {
   closeModal();
 });
 
-if(modalNoButton) {
+if (modalNoButton) {
   modalNoButton.addEventListener('click', closeModal);
 }
-
 
 function closeModal() {
   // modal.style.display = 'none';
   // backdrop.style.display = 'none';
-  if(modal) {
+  if (modal) {
     modal.classList.remove('open');
   }
   backdrop.classList.remove('open');
+  setTimeout(function () {
+    backdrop.style.display = 'none';
+  }, 200);
 }
 
 toggleButton.addEventListener('click', function () {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add('open');
-  backdrop.classList.add('open');
+  backdrop.style.display = 'block';
+  setTimeout(function () {
+    backdrop.classList.add('open');
+  }, 200);
 });
